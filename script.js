93bf2707aeea5861da2ad881b0bba446b9624996
mvularomeomartin-dev <mvularomeomartin@gmail.com>
2025-12-30 22:28:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+  // Smooth scrolling
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Progress bar
+window.addEventListener('scroll', () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector('.progress-fill').style.width = scrolled + '%';
+});
